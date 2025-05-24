783962bcdb01aa4f09aa83bf39c0fc9d1bfdfcd5
Your Name <you@example.com>
2025-05-25 02:26:29 +0800

    doing first basic frontend design(background; login; register; stack of books)

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#" className="logo">Bookies</a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header> 
+      <div className="post">
+        <img 
+          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
+          alt="stack of books picture"
+        />
+        <h2>
+          The Perks of being a Wallflower
+        </h2>
+        <p>
+          why so depressing bro...
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
