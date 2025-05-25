84387d6328d392c73662234d98b9b4d07f0b81f5
Your Name <you@example.com>
2025-05-26 02:40:27 +0800

    Establishing first draft of navigating from home; login; register; pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,18 @@
 import './App.css';
+import Post from './Post';
+import Header from './Header';
+import Layout from './Layout';
+import {Route, Routes} from "react-router-dom"
+import HomePage from './HomePage';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="#" className="logo">Bookies</a>
-        <nav>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
-        </nav>
-      </header> 
-      <div className="post">
-        <div className="image">
-          <img 
-          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
-          alt="stack of books picture"
-        />
-        </div>
-        <div className="texts">
-           <h3>The Russian Revolution</h3>
-           <p>very educational</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img 
-          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
-          alt="stack of books picture"
-        />
-        </div>
-        <div className="texts">
-           <h3>The Perks of being a Wallflower</h3>
-           <p>why so depressing bro...</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img 
-          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
-          alt="stack of books picture"
-        />
-        </div>
-        <div className="texts">
-           <h3>All the light we cannot see</h3>
-           <p>once again, why so depressing bro...</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path = "/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path ={'/login'} element = {<div>login page</div>} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,16 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+            <img 
+            src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
+            alt="stack of books picture"
+            />
+            </div>
+            <div className="texts">
+                <h3>The Russian Revolution</h3>
+                <p className="review">The Russian Revolution: A New History is a political history of the Russian Revolution written by Sean McMeekin and published by Basic Books in 2017. The release was timed with the 100th anniversary of the Russian Revolution.</p>
+            </div>
+        </div>
+    );
+}
