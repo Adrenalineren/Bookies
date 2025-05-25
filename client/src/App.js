393d0670200abda09e9f2f9169e8ca33891165b4
Your Name <you@example.com>
2025-05-26 03:10:29 +0800

    First draft for HomePage, LoginPage, and RegisterPage

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import Header from './Header';
 import Layout from './Layout';
 import {Route, Routes} from "react-router-dom"
 import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path = "/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path ={'/login'} element = {<div>login page</div>} />
+        <Route path ={'/login'} element = {<LoginPage />} />
+        <Route path ={'/Register'} element = {<RegisterPage/>}/>
       </Route>
     </Routes>
   );
