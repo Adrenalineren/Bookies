5798bd898ab25833e0902c76beee6321f2bd94b5
Your Name <you@example.com>
2025-05-25 23:23:11 +0800

    First draft of home page

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,40 @@ function App() {
         </nav>
       </header> 
       <div className="post">
-        <img 
+        <div className="image">
+          <img 
           src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
           alt="stack of books picture"
         />
-        <h2>
-          The Perks of being a Wallflower
-        </h2>
-        <p>
-          why so depressing bro...
-        </p>
+        </div>
+        <div className="texts">
+           <h3>The Russian Revolution</h3>
+           <p>very educational</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img 
+          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
+          alt="stack of books picture"
+        />
+        </div>
+        <div className="texts">
+           <h3>The Perks of being a Wallflower</h3>
+           <p>why so depressing bro...</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img 
+          src="https://img.pikbest.com/wp/202435/of-books-and-opened-book-on-the-top-cartoon-illustration_10770955.jpg!bw700"
+          alt="stack of books picture"
+        />
+        </div>
+        <div className="texts">
+           <h3>All the light we cannot see</h3>
+           <p>once again, why so depressing bro...</p>
+        </div>
       </div>
     </main>
   );
