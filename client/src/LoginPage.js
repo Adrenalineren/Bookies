dc2fa7d5e247117c49737045107420fafbc7dbf0
Sheryee <sheryeefong@gmail.com>
2025-05-26 14:07:15 +0800

    Implemented authentication feature on the Register page

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,9 +1,15 @@
+import {useState} from "react";
+
 export default function LoginPage() {
     return (
         <form className="login">
             <h1>Login</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" 
+                placeholder="username" 
+            />
+            <input type="password" 
+                placeholder="password" 
+            />
             <button>Login</button>
         </form>
     );
