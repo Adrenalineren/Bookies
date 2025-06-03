13cae3234eebbcb8e7ea99c3ef223396e8615b7e
Your Name <you@example.com>
2025-06-03 23:40:59 +0800

    Improved Create New Book page

--- client/src/CreateBook.js
+++ client/src/CreateBook.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
 export default function CreateBook() {
+    const [title,setTitle] = useState('');
+    const [details,setDetails] = useState('');
+    const [content,setContent] = useState('');
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'indent': '-1'}, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['clean']
+        ]
+    };
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image',
+    ];
+
     return(
         <form>
             <input type="title" placeholder="Title"/>
-            <input type="review" placeholder="Review"/>
+            <input type="details" placeholder="Details"/>
+            <input type="file"/>
+            <ReactQuill value = {content} modules={modules} formats={formats}/>
+            <button style={{marginTop:'5px'}}>Done</button>
         </form>
     )
 }
