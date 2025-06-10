d75b2a58549bdc920e6222fc8874cdf16c538b21
Sheryee <sheryeefong@gmail.com>
2025-06-10 15:09:23 +0800

    Add search bar

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,21 +1,43 @@
 import Post from "./Post";
+import SearchBar from "./components/SearchBar";
 import { useState, useEffect } from "react";
 
 export default function HomePage() {
     const [posts, setPosts] = useState([]);
+    const [query, setQuery] = useState('');
+    const [filteredPosts, setFilteredPosts] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:4000/post').then(response => {
             response.json().then(posts => {
-                setPosts(posts)
+                setPosts(posts);
+                setFilteredPosts(posts);
             });
         });
     }, []);
+
+    const handleSearch = () => {
+        const lowerQuery = query.toLowerCase();
+        const result = posts.filter(post =>
+        post.title.toLowerCase().includes(lowerQuery)
+        );
+        setFilteredPosts(result);
+  };
+
     return (
         <>
-            {posts.length > 0 && posts.map(post => (
-                <Post {...post} />    
-            ))}
+        <SearchBar value={query} onChange={setQuery} onSearch={handleSearch}/>
+            {filteredPosts.length > 0 ? (filteredPosts.map(post =>(
+                <Post key={post._id} {...post} />
+            ))
+        ) : (
+            <p>No books found</p>
+        )}
         </>
     );
 }
+/*
+{posts.length > 0 && posts.map(post => (
+                <Post {...post} />    
+            ))}
+*/
